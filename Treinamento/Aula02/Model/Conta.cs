febffdbcc82d68aaf77454fe94db50da2c9fa8ca
Sergio Fernando Vicente <sergio.vicente@gatec.com.br>
2017-12-12 20:37:40 -0200

    Aula2

---
+++ Treinamento/Aula02/Model/Conta.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula02.Model
+{
+    public abstract class Conta
+    {
+        public int Agencia { get; set; }
+
+        public int Numero { get; set; }
+
+        public decimal Saldo { get; set; }
+
+        private List<Item> _extrato = new List<Item>();
+
+        public  void Depositar(decimal valor)
+        {
+            Saldo += valor;
+
+            _extrato.Add(new Item()
+            {
+                Data = DateTime.Now,
+                Valor = valor 
+            });
+        }
+
+        public  void Sacar(decimal valor)
+        {
+            Saldo -= valor;
+
+            _extrato.Add(new Item()
+            {
+                Data = DateTime.Now,
+                Valor = valor * -1
+            });
+        }
+
+        public abstract void Movimentar();
+
+        public Item[] ObterExtrato()
+        {
+            return _extrato.ToArray();
+        }
+
+
+
+
+
+
+    }
+}
