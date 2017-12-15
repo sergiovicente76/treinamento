fecb90ac3f594e14ec6c5e69a9ac3a9fecd24312
Sergio Fernando Vicente <sergio.vicente@gatec.com.br>
2017-12-14 23:49:28 -0200

    commit five

--- Treinamento/Aula02/Program.cs
+++ Treinamento/Aula02/Program.cs
@@ -22,7 +22,7 @@ namespace Aula02
             foreach(Item item in extrato)
             {
 
-                Console.ForegroundColor = item.Valor < 0 ? ConsoleColor.Red : ConsoleColor.Gray;
+                Console.ForegroundColor = item.Valor < 0 ? ConsoleColor.Red : ConsoleColor.Blue;
 
                 /*
                 if (item.Valor < 0 || item.Valor == 0)
@@ -39,9 +39,89 @@ namespace Aula02
             }
 
             Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("---------------------------------------");
+
+        }
+
+        static void Bomba(int kt)
+        {
+            if (kt > 110)
+            throw new ArgumentOutOfRangeException("kt", "Nao Existe bomba mais forte que Ksar");
         }
         static void Main(string[] args)
         {
+
+            var name = "Sergio Fernando";
+
+            var a = name.Split(" ");
+
+            var b = string.Join(";", a);
+
+            var fullName = String
+
+            int[] numbers = { 1, 2, 3, 4, 5 };
+
+            string a = "Letra A";
+            string b = "Letra B";
+            string c = a + b;
+
+            a = a + "  ABC";
+
+            a.
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("Sergio");
+            sb.Append(" ");
+            sb.Append("Fernando");
+            sb.Insert(0, "Sr.");
+
+            Console.WriteLine(sb.ToString());
+
+
+            try
+            {
+                Console.WriteLine("Iniciando programa...");
+                Bomba(80);
+                Console.WriteLine("Finalizando bloco Try...");
+            }
+            catch(ArgumentOutOfRangeException e)
+            {
+                
+                Console.WriteLine($"Parametro {e.ParamName} : {e.Message}");
+            }
+            catch(IndexOutOfRangeException e)
+            {
+                Console.WriteLine("Indice fora do array.");
+            }
+           
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                //throw;
+            }
+            finally
+            {
+                Console.WriteLine("Programa Excecutado");
+            }
+
+            /*
+            int i = 10;
+            Console.Writeline("Input a number:");
+            int n = int.Parese(Console.ReadLine());
+
+            if(i == n && i < n)
+            {
+
+            }
+            else if (i != n)
+            {
+
+            }
+            */
+            
+            
+            /*
             Conta c = new ContaCorrente(0145, 0121065);
 
             c.Depositar(100m);
@@ -52,7 +132,7 @@ namespace Aula02
             ImprimirExtrato(c);
 
 
-
+    */
             Console.ReadKey();
         }
     }
